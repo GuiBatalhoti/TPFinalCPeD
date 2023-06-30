39efb4a5ea37fd0e5fe6f30445cc821d94aaf6eb
GuiBatalhoti <guibatalhoti@gmail.com>
2023-06-30 13:40:15 -0300

    change to encript text, comments, remove compress

--- client/ComputeCode.java
+++ client/ComputeCode.java
@@ -6,31 +6,34 @@ import compute.Compute;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.PrintWriter;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Scanner;
 
 public class ComputeCode {
 
-    private static BufferedImage img;
+    private static String name = "Compute";
 
     /*
-     * @param args the command line arguments (0: the server ip; 1: the server port, 2:the image path)
+     * @param args the command line arguments (0: the server ip; 1: the server port, 2:the image path, 3:the text to encrypt path)
      */
     public static void main(String[] args) {
 
+        // main loop
         while (true) {
-            int option = menuLoop();
+            int option = menu();
             switch (option) {
                 case 0:
                     System.out.println("Exiting...");
                     System.exit(0);
                     break;
                 case 1:
-                    img = openImage(args[2]);
-                    executeDIP(img, args[0], args[1]);
+                    executeDIP(args[2], args[0], args[1]);
                     break;
                 case 2:
-                    executeCryptography(args[2], args[0], args[1]);
+                    executeCryptography(args[3], args[0], args[1]);
+                    break;
                 case 3:
                     executeCompression(args[2], args[0], args[1]);
                     break;
@@ -41,10 +44,11 @@ public class ComputeCode {
         }
     }
 
-    private static int menuLoop() {
+    private static int menu() {
+        //menu wwith options
         System.out.println("Choose an option:");
         System.out.println("1 - DIP (Laplacian Gaussian)");
-        System.out.println("2 - Cryptography (Encrypt the image)");
+        System.out.println("2 - Cryptography (Encrypt text file)");
         System.out.println("3 - Compression (Compress the image)");
         System.out.println("0 - Sair");
         System.out.print("Opção: ");
@@ -52,36 +56,62 @@ public class ComputeCode {
         return option;
     }
 
-    private static void executeDIP(BufferedImage img, String ip, String port) {
+    private static void executeDIP(String imgPath, String ip, String port) {
+        //execute laplacian gaussian filter in image
+
+        //set security manager
         if (System.getSecurityManager() == null) {
             System.setSecurityManager(new SecurityManager());
         }
         try {
-            String name = "Compute";
+            //get registry
             Registry registry = LocateRegistry.getRegistry(ip,Integer.parseInt(port));
-            Compute comp = (Compute) registry.lookup(name);
+            //get compute engine
+            Compute computeEng = (Compute) registry.lookup(ComputeCode.name);
+
+            //open image
+            BufferedImage img = openImage(imgPath);
+            //convert image to byte array, so it can be sent to the server
             byte[] imgBytes = DIP.bufferedImageToByteArray(img);
+            //create task
             DIP task = new DIP(imgBytes);
-            byte[] transformedImgBytes = comp.executeTask(task);
+            //execute task and get the transformed image in byte array form
+            byte[] transformedImgBytes = computeEng.executeTask(task);
+            //convert byte array to image
             BufferedImage transformedImg = DIP.byteArrayToBufferedImage(transformedImgBytes);
+            //save output image
             saveImage(transformedImg);
+            System.out.println("Image saved successfuly into \"image_borders.png\".");
+            System.out.println("\n\n");
         } catch (Exception e) {
             System.err.println("ComputeDIP exception:");
             e.printStackTrace();
         }
     }
 
-    private static void executeCryptography(String imagePath, String ip, String port) {
+    private static void executeCryptography(String textPath, String ip, String port) {
+        //execeute cryptography in text file
+
+        //set security manager
         if (System.getSecurityManager() == null) {
             System.setSecurityManager(new SecurityManager());
         }
         try {
-            String name = "Compute";
+            //get registry
             Registry registry = LocateRegistry.getRegistry(ip,Integer.parseInt(port));
-            Compute comp = (Compute) registry.lookup(name);
-            Cryptography task = new Cryptography(imagePath);
-            String encryptedImagePath = comp.executeTask(task);
-            System.out.println("Image encrypted successfully onto \"" + encryptedImagePath + "\".");
+            //get compute engine
+            Compute computeEng = (Compute) registry.lookup(ComputeCode.name);
+
+            //get text from file
+            String text = openText(textPath);
+            //create task
+            Cryptography task = new Cryptography(text);
+            //get the encrypted text
+            String encryptedText = computeEng.executeTask(task);
+            //save encrypted text
+            saveText(encryptedText);
+            System.out.println("Text saved successfuly into \"text_encrypted.txt\".");
+            System.out.println("\n\n");
         } catch (Exception e) {
             System.err.println("ComputeCryptography exception:");
             e.printStackTrace();
@@ -89,28 +119,69 @@ public class ComputeCode {
     }
 
     private static void executeCompression(String imagePath, String ip, String port) {
+        //execeute cryptography in text file
+
+        //set security manager
         if (System.getSecurityManager() == null) {
             System.setSecurityManager(new SecurityManager());
         }
         try {
-            String name = "Compute";
+            //get registry
             Registry registry = LocateRegistry.getRegistry(ip,Integer.parseInt(port));
-            Compute comp = (Compute) registry.lookup(name);
+            //get compute engine
+            Compute computeEng = (Compute) registry.lookup(ComputeCode.name);
+
+            //create task
             CompressImage task = new CompressImage(imagePath);
-            String compressedImagePath = comp.executeTask(task);
+            String compressedImagePath = computeEng.executeTask(task);
             System.out.println("Image compressed successfully onto \"" + compressedImagePath + "\".");
+            System.out.println("\n\n");
         } catch (Exception e) {
             System.err.println("ComputeCompression exception:");
             e.printStackTrace();
         }
     }
 
-    private static BufferedImage openImage(String path) {
-        System.out.println("\n\nOpening Image...");
+    private static String openText(String path) {
+        System.out.println("\nOpening Text file...");
         try {
+            //open text file
             File arquivo = new File(path);
+            //read text file
+            Scanner scanner = new Scanner(arquivo);
+            //get text
+            String text = scanner.nextLine();
+            //close text file
+            scanner.close();
+            System.out.println("Text file opened successfuly.\n");
+            return text;
+        } catch (Exception e) {
+            System.err.println("Error opening text file.\n");
+            return null;
+        }
+    }
+
+    private static void saveText(String text) {
+        try {
+            //save text file
+            File arquivo = new File("text_encrypted.txt");
+            //write text file
+            PrintWriter writer = new PrintWriter(arquivo);
+            writer.println(text);
+            writer.close();
+        } catch (Exception e) {
+            System.err.println("Error saving text file.");
+        }
+    }
+
+    private static BufferedImage openImage(String path) {
+        System.out.println("Opening Image...");
+        try {
+            //open image
+            File arquivo = new File(path);
+            //read image
             BufferedImage img = ImageIO.read(arquivo);
-            System.out.println("Image opened successfuly.\n\n");
+            System.out.println("Image opened successfuly.\n");
             return img;
         } catch (Exception e) {
             System.err.println("Error opening image.\n");
@@ -120,9 +191,10 @@ public class ComputeCode {
 
     private static void saveImage(BufferedImage img) {
         try {
+            //save image
             File arquivo = new File("image_borders.png");
+            //write image
             ImageIO.write(img, "png", arquivo);
-            System.out.println("Image saved successfuly into \"image_borders.png\".");
         } catch (Exception e) {
             System.err.println("Error saving image.");
         }
--- client/Cryptography.java
+++ client/Cryptography.java
@@ -2,10 +2,12 @@ package client;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.InvalidKeyException;
+import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 
 import javax.crypto.BadPaddingException;
@@ -19,79 +21,42 @@ import compute.Task;
 
 public class Cryptography implements Task<String>, Serializable {
     
-    private String imagePath;
-    private String encryptedImagePath;
+    private String text;
+    private String encryptedText;
     private String key;
 
     @Override
     public String execute() {
         System.out.println("\n\nExecuting Cryptography...");
-        encryptImage(this.imagePath, this.encryptedImagePath, this.key);
+        this.encryptedText = encryptText(text, key);
+        if (this.encryptedText == null) {
+            System.out.println("Error encrypting text.");
+            return null;
+        }
         System.out.println("Cryptography executed.\n\n");
-        return this.encryptedImagePath;
+        return this.encryptedText;
     }
 
-    public Cryptography(String imagePath) {
-        this.imagePath = imagePath;
-        this.encryptedImagePath = "imagem_encrypted.png";
+    public Cryptography(String text) {
+        this.text = text;
         this.key = "0123456789123456";
     }
 
-    public static void encryptImage(String imagePath, String encryptedImagePath, String key) {
+    public static String encryptText(String text, String key) {
         try {
-            System.out.println("Encrypting image...");
-            // Lê a imagem em um array de bytes
-            byte[] imageBytes = Files.readAllBytes(Paths.get(imagePath));
-
-            // Gera uma chave secreta baseada na chave fornecida
-            SecretKey secretKey = generateSecretKey(key);
-
-            // Cria um objeto de cifra para criptografia
+            System.out.println("Encrypting text...");
             Cipher cipher = Cipher.getInstance("AES");
+            SecretKey secretKey = generateSecretKey(key);
             cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-
-            // Criptografa o array de bytes da imagem
-            byte[] encryptedBytes = cipher.doFinal(imageBytes);
-
-            // Escreve os bytes criptografados em um novo arquivo
-            Path encryptedPath = Paths.get(encryptedImagePath);
-            Files.write(encryptedPath, encryptedBytes);
-
-            System.out.println("Image encryption completed.");
-
-        } catch (IOException | NoSuchAlgorithmException | NoSuchPaddingException
-                | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+            System.out.println("Text encrypted.");
+            byte[] encryptedBytes = cipher.doFinal(text.getBytes(StandardCharsets.UTF_8));
+            return new String(encryptedBytes, StandardCharsets.UTF_8);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
             e.printStackTrace();
         }
+        return null;
     }
-
-    public static void decryptImage(String encryptedImagePath, String decryptedImagePath, String key) {
-        try {
-            // Read the encrypted image file into a byte array
-            byte[] encryptedBytes = Files.readAllBytes(Paths.get(encryptedImagePath));
-
-            // Generate a secret key based on the provided key
-            SecretKey secretKey = generateSecretKey(key);
-
-            // Create a cipher object for decryption
-            Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.DECRYPT_MODE, secretKey);
-
-            // Decrypt the encrypted byte array
-            byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
-
-            // Write the decrypted bytes to a new file
-            Path decryptedPath = Paths.get(decryptedImagePath);
-            Files.write(decryptedPath, decryptedBytes);
-
-            System.out.println("Image decryption completed.");
-
-        } catch (IOException | NoSuchAlgorithmException | NoSuchPaddingException
-                | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
-            e.printStackTrace();
-        }
-    }
-
+    
     public static SecretKey generateSecretKey(String key) throws NoSuchAlgorithmException {
         // Generate a 128-bit AES secret key from the provided key bytes
         byte[] keyBytes = key.getBytes();
